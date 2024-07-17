26732cde72e553b34d49cfb7e2c2166c6337f835
C4l3b-N4v4rr0 <nkaleb74@gmail.com>
2024-07-17 12:09:40 -0600

    nuevo

---
+++ php/control-maestros.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <script src="js/jquery.js" type="text/javascript"></script>
+
+  <title>Control escolar </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+   <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="../index.php" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">UNEDL</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item dropdown">
+
+        <li class="nav-item dropdown pe-3">
+            <!-- usar un if , si es el perfil de caleb o el mio   -->
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">C. Navarro</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Caleb Navarro</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li>
+                <a class="dropdown-item d-flex align-items-center" href="php/cerrar.php">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Salir</span>
+                </a>
+            </li>
+            <!-- termina el if -->
+            
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav>
+
+  </header><!-- End Header -->
+
+<!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="../index.php">
+          <i class="bi bi-grid"></i>
+          <span>Menu</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Alumnos</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="control-registro.php">
+              <i class="bi bi-circle"></i><span>Registro</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Components Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-journal-text"></i><span>Docentes</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="Control-Maestros.php">
+              <i class="bi bi-circle"></i><span>Control Docentes</span>
+            </a>
+          </li>
+          <li>
+            <a href="materias-docentes.php">
+              <i class="bi bi-circle"></i><span>Asignación de materias a docentes</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Forms Nav -->
+
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-bar-chart"></i><span>Materias</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="control-materias.php">
+              <i class="bi bi-circle"></i><span>Registro de materias</span>
+            </a>
+          </li>
+          <li>
+            <a href="control-horarios-materias.php">
+              <i class="bi bi-circle"></i><span>Control de horarios de materias</span>
+            </a>
+          </li>
+          <li>
+            <a href="control-calificaciones.php">
+              <i class="bi bi-circle"></i><span>Control de calificaciones</span>
+            </a>
+          </li>
+          <li>
+            <a href="imprimir-boleta.php">
+              <i class="bi bi-circle"></i><span>Impresión de boleta</span>
+            </a>
+          </li> 
+          <li>
+            <a href="imprimir-horario.php">
+              <i class="bi bi-circle"></i><span>Impresión de horario de clases</span>
+            </a>
+          </li>          
+        </ul>
+      </li><!-- End Charts Nav -->
+      
+      
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-layout-text-window-reverse"></i><span>Pagos</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="pago.php">
+              <i class="bi bi-circle"></i><span>Pago inmediato</span>
+            </a>
+          </li>
+          <li>
+            <a href="pago-parcial.php">
+              <i class="bi bi-circle"></i><span>Pago parcial</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Tables Nav -->
+     
+
+      <li class="nav-heading">Informacion</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile.php">
+          <i class="bi bi-person"></i>
+          <span>Usuario</span>
+        </a>
+      </li><!-- End Profile Page Nav-aqui un if si es mi perfil o el de  -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="info-nuevo-ingreso.php">
+          <i class="bi bi-question-circle"></i>
+          <span>Datos necesarios para ingreso</span>
+        </a>
+      </li><!-- End F.A.Q Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-contact.php">
+          <i class="bi bi-envelope"></i>
+          <span>Contacto</span>
+        </a>
+      </li><!-- End Contact Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="becas.php">
+          <i class="bi bi-card-list"></i>
+          <span>Becas</span>
+        </a>
+      </li><!-- End Register Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="servicio-social.php">
+          <i class="bi bi-dash-circle"></i>
+          <span>Servicio Social</span>
+        </a>
+      </li><!-- End Error 404 Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="practicas.php">
+          <i class="bi bi-file-earmark"></i>
+          <span>Practicas</span>
+        </a>
+      </li><!-- End Blank Page Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+  
+  
+  
+  
+  
+  
+  <!-- AQUI COMINEZA EL FORM-->
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Control de Docentes</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Menu</a></li>
+          <li class="breadcrumb-item">Docentes</li>
+          <li class="breadcrumb-item active">Control de Docentes</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Informacion de Docentes </h5>
+
+              <!-- General Form Elements -->
+              <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">CURP</th>
+      <th scope="col">RFC</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Miriam</td>
+      <td>Lopez de Alba</td>
+      <td>MLDA123</td>
+      <td>MIRI123</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Roberto</td>
+      <td>Estrada Rosas</td>
+      <td>RER123</td>
+      <td>ROB123</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Edgar</td>
+      <td>Hernandez Barajas</td>
+      <td>EHB123</td>
+      <td>EDG123</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Alan</td>
+      <td>Reinaga Curiel</td>
+      <td>ARC123</td>
+      <td>ALA123</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Emmanuel</td>
+      <td>Flores Ramirez</td>
+      <td>EMR123</td>
+      <td>EMM123</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Jorge</td>
+      <td>Vazquez Abundis</td>
+      <td>JVA123</td>
+      <td>JOR123</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Carlos</td>
+      <td>Vazquez Uribe</td>
+      <td>CVU123</td>
+      <td>CAR123</td>
+    </tr>
+    
+  </tbody>
+</table>
+             
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>KletJR</span></strong>.<br>
+      All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      Designed by <a href="https://www.instagram.com/sergio_cnp/">Klet </a>
+      <span class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      and <a href="https://www.instagram.com/julirmx_mx/">JR</a>
+    </span>
+    </div>
+      
+  </footer><!-- End Footer -->
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="../assets/vendor/echarts/echarts.min.js"></script>
+  <script src="../assets/vendor/quill/quill.js"></script>
+  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
