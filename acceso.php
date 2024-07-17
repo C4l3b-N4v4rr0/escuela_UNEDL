00c7f88395361ed8a12fb83c48a2e640a826de9c
C4l3b-N4v4rr0 <nkaleb74@gmail.com>
2024-07-17 13:03:31 -0600

    caleb

--- acceso.php
+++ acceso.php
@@ -68,7 +68,7 @@ if(isset($_SESSION['username'])==true && $_SESSION['tipo']==2){
                 <div class="card-body">
 
                   <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Inicia sesion con tu cuenta</h5>
+                    <h5 class="card-title text-center pb-0 fs-4">Inicia sesion con tu cuentaa</h5>
                     <p class="text-center small">Ingresa tu usuario y tu contraseña para iniciar</p>
                   </div>
 
