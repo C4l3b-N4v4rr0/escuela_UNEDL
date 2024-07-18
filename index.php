d91bf990879be88a44a081c5ff90f5f2cd498d1e
juls1205 <julianaramirez272@gmail.com>
2024-07-18 08:29:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -62,17 +62,17 @@ if(isset($_SESSION['username'])==false){
       }
 
       .bg-primary {
-        background-color: #007bff !important;
+        background-color: ##00a2ff !important;
       }
 
       .text-white {
         color: #fff !important;
       }
       .bg-secondary {
-        background-color: #13d825 !important;
+        background-color: ##fb2440 !important;
       }
       .bg-warning {
-        background-color: #ffc107 !important;
+        background-color: ##68ddbd !important;
       }
 
       .text-white {
@@ -96,43 +96,86 @@ if(isset($_SESSION['username'])==false){
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
+    <?php
+      if (isset($_SESSION['username'])=='caleb'){
+        <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
+  
+          <li class="nav-item dropdown">
+  
+          <li class="nav-item dropdown pe-3">
+              <!-- usar un if , si es el perfil de caleb o el mio   -->
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+              <img src="assets/img/perfilcaleb.jpg" alt="Profile" class="rounded-circle">
+              <span class="d-none d-md-block dropdown-toggle ps-2">C. Navarro</span>
+            </a><!-- End Profile Iamge Icon -->
+  
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+              <li class="dropdown-header">
+                <h6>Caleb Navarro</h6>
+                <span>Web Designer</span>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li>
+                  <a class="dropdown-item d-flex align-items-center" href="php/cerrar.php">
+                  <i class="bi bi-box-arrow-right"></i>
+                  <span>Salir</span>
+                  </a>
+              </li>
+              <!-- termina el if -->
+              
+            </ul><!-- End Profile Dropdown Items -->
+          </li><!-- End Profile Nav -->
+  
+        </ul>
+      </nav>
+  
+    </header><!-- End Header -->
+  
+
+      }else if(isset($_SESSION['username'])=='July'){
+        <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
+  
+          <li class="nav-item dropdown">
+  
+          <li class="nav-item dropdown pe-3">
+              <!-- usar un if , si es el perfil de caleb o el mio   -->
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+              <img src="assets/img/perfiljuli.jpg" alt="Profile" class="rounded-circle">
+              <span class="d-none d-md-block dropdown-toggle ps-2">J. Ramirez</span>
+            </a><!-- End Profile Iamge Icon -->
+  
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+              <li class="dropdown-header">
+                <h6>Juliana Ramirez</h6>
+                <span>Web Designer</span>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li>
+                  <a class="dropdown-item d-flex align-items-center" href="php/cerrar.php">
+                  <i class="bi bi-box-arrow-right"></i>
+                  <span>Salir</span>
+                  </a>
+              </li>
+              <!-- termina el if -->
+              
+            </ul><!-- End Profile Dropdown Items -->
+          </li><!-- End Profile Nav -->
+  
+        </ul>
+      </nav>
+  
+    </header><!-- End Header -->
+
+      }?>
+
     
 
-    <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
-
-        <li class="nav-item dropdown">
-
-        <li class="nav-item dropdown pe-3">
-            <!-- usar un if , si es el perfil de caleb o el mio   -->
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/perfilcaleb.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">C. Navarro</span>
-          </a><!-- End Profile Iamge Icon -->
-
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-              <h6>Caleb Navarro</h6>
-              <span>Web Designer</span>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li>
-                <a class="dropdown-item d-flex align-items-center" href="php/cerrar.php">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Salir</span>
-                </a>
-            </li>
-            <!-- termina el if -->
-            
-          </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
-
-      </ul>
-    </nav>
-
-  </header><!-- End Header -->
 
 <!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
